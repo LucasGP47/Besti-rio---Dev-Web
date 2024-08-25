f7a04ab2ab6fc7f6b3001806ce3a15b9d529b3b7
LucasGP47 <111434296+LucasGP47@users.noreply.github.com>
2024-08-25 12:58:42 -0300

    Delete Bestiário - Deb Web/scripts.js

--- Bestiário - Deb Web/scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btnConstrutos').addEventListener('click', function() {
-    const construtosList = document.getElementById('construtosList');
-    construtosList.classList.toggle('hidden');
-});
-
-document.getElementById('btnDraconideos').addEventListener('click', function() {
-    const draconideosList = document.getElementById('draconideosList');
-    draconideosList.classList.toggle('hidden');
-});
